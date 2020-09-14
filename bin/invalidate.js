5205113b16214fda3a05bf2e5c1fceecba17ec44
Kevin <kevin.isom@gmail.com>
2020-09-15 10:55:40 +1200

    Handle different options for params to invalidate better

--- bin/invalidate.js
+++ bin/invalidate.js
@@ -10,14 +10,19 @@ program
   .action(async (cmd, opts) => {
     try {
       console.info('Prepare to invalidate', opts)
-
       const doc = yaml.safeLoad(
         fs.readFileSync(path.join(process.cwd(), 'launchpad.yml'), 'utf8')
       )
 
       const { distribution } = doc.env[process.env.NODE_ENV || 'dev']
 
-      await invalidate((opts && opts[0]) || distribution)
+      if (distribution) {
+        await invalidate(distribution)
+      } else if (opts && opts[0]) {
+        await invalidate(opts[0])
+      } else {
+        console.warn('No Distribution ID to invalidate')
+      }
     } catch (e) {
       console.error('Failed to upload', e)
     }
